b0852e773dbf1d76f0d1d44fe445b5ce160bbe68
Patryk Krawczyk <patrykkrawczykk157@gmail.com>
2025-05-19 21:40:29 +0200

    adding styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Components/Shared/App.js";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./Components/ReactCalendar/styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
